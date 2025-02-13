072df72cf0db15f726791dfe15b6b04d448d0f75
Ethan <ethanadiem@gmail.com>
2025-02-13 16:26:32 -0700

    Created buttons and numbers to align with eachother on the block

--- index.js
+++ index.js
@@ -1,17 +1,25 @@
 document.addEventListener("DOMContentLoaded", function() {
-  const buttonContainer = document.getElementById("buttons-container");
-  const numberContainer = document.getElementById("number-container");
+  const rectangle = document.getElementById("number-container");
 
-  for(let n = 1; n <= 10; i++) {
-    let holesButton = document.createElement("button");
-    holesButton.classList.add("button");
-    button.addEventListener("click", function() {
-      //functionality of the button holes
-    });
-    buttonContainer.appendChild(holesButton);
+  let buttonContainer = document.createElement("div");
+  buttonContainer.classList.add("buttons-container");
+  rectangle.appendChild(buttonContainer);
 
-    let numsToTen = document.createElement("span");
-        numsToTen.innerText = i;
-        numberContainer.appendChild(numsToTen);
-    }
+  let numbersContainer = document.createElement("div");
+  numbersContainer.classList.add("numbers-container");
+  rectangle.appendChild(numbersContainer);
+
+  for (let n = 1; n <= 10; n++) {
+      let holesButton = document.createElement("button");
+      holesButton.classList.add("button");
+      holesButton.innerText = `Button ${n}`;
+      holesButton.addEventListener("click", function() {
+          alert(`You clicked Button ${n}`);
+      });
+      buttonContainer.appendChild(holesButton);
+
+      let numsToTen = document.createElement("span");
+      numsToTen.innerText = n;
+      numbersContainer.appendChild(numsToTen);
+  }
 });
